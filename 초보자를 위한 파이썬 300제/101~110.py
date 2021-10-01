1d505cce21919bfdbd63923495078612346f3046
Myung <miung4130@naver.com>
2021-10-01 15:59:03 +0900

    python

---
+++ 초보자를 위한 파이썬 300제/101~110.py
@@ -0,0 +1,33 @@
+#초보자를 위한 파이썬 300제
+
+print(type(True), type(False))
+print(3==5)
+print(3<5)
+x = 4
+print(1<x<5)
+print((3==3)and(4!=3))
+print(3>=4)
+
+if 4 < 3:
+    print("hello world")
+else:
+    print("hi, there")
+
+print("\n")
+if True:
+    print("1")
+    print("2")
+else:
+    print("3")
+print("4")
+
+print("\n")
+if True:
+    if False:
+        print("1")
+        print("2")
+    else:
+        print("3")
+else:
+    print("4")
+print("5")
